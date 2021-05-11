dd8447747a86a372cb94aacce2c7fada138ea520
Ananda Gurgel <gurgel.ananda@gmail.com>
2021-05-11 17:25:50 -0300

    Ajustando o style da tela

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let food = {
 }
 
 function createBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "white";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
@@ -43,6 +43,13 @@ function startGame() {
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert('Game Over !! ');
+        }
+    }    
 
     createBG();
     createSnake();
